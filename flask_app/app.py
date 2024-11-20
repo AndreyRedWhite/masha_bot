0b90372e95e849cf26e713ac227f7a0bc67c49a2
AndreyRedWhite <andreyredwhite@gmail.com>
2024-11-20 18:38:05 +0300

    more

--- flask_app/app.py
+++ flask_app/app.py
@@ -29,6 +29,7 @@ def verify_password(username, password):
     if username in users and check_password_hash(users.get(username), password):
         return username
 
+
 # Путь к файлу данных
 DATA_FILE = os.getenv("DATA_FILE", "/app/shared_data/data.json")
 
